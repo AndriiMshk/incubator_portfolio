3df640c23e69e54ba829b31ec9ecad5e31017be6
AndrMshk <andrmshk1@gmail.com>
2022-11-07 22:34:46 +0200

    fix margin

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -9,32 +9,30 @@ import Tilt from 'react-vanilla-tilt';
 export const Main = () => {
   const [hover, setHover] = useState(false);
   return (
-    <>
-      <main
-        onMouseEnter={() => setHover(true)}
-        onMouseLeave={() => setHover(false)}
-        className={style.main}
-        id={'main'}
-      >
-        <div className={`${container.container} ${style.container}`}>
-          <div className={style.photo}>
-            <Tilt>
-              <img src={ava} alt="ava"  />
-            </Tilt>
+    <main
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+      className={style.main}
+      id={'main'}
+    >
+      <div className={`${container.container} ${style.container}`}>
+        <div className={style.photo}>
+          <Tilt>
+            <img src={ava} alt="ava" />
+          </Tilt>
+        </div>
+        <div className={style.greeting}>
+          <div className={style.title}>
+            <Title title={'About me'} hover={hover} />
           </div>
-          <div className={style.greeting}>
-            <div className={style.title}>
-              <Title title={'About me'} hover={hover} />
-            </div>
-            <div>
-              <h1>Andrii Moshko</h1>
-              <TypeIt style={{ fontSize: '1.17em', fontWeight: 'bold' }} options={{ loop: true }}>
-                Front-end Developer
-              </TypeIt>
-            </div>
+          <div>
+            <h1>Andrii Moshko</h1>
+            <TypeIt style={{ fontSize: '1.17em', fontWeight: 'bold' }} options={{ loop: true }}>
+              Front-end Developer
+            </TypeIt>
           </div>
         </div>
-      </main>
-    </>
+      </div>
+    </main>
   );
 };
